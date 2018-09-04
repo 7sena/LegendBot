0812d42225f5781a66f42c1c65de6a434770d7d2
xBacose <42037258+xBacose@users.noreply.github.com>
2018-09-04 23:53:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require('fs');
-const prefix = '$';
+const prefix = "$";
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
-
-client.login(process.env.BOT_TOKEN);
+  client.login(process.env.BOT_TOKEN);
