6b685722d53dc708e6dba657cd05cb5de369e138
xBacose <42037258+xBacose@users.noreply.github.com>
2018-09-04 23:50:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,11 +7,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === prefix + 'ping') {
-    msg.channel.send('Pinging...').then(sent => {
-      sent.edit(`Pong! Took ${sent.createdTimestamp - msg.createdTimestamp}ms`);
-  });
 
 
 client.login(process.env.BOT_TOKEN);
